5fa8ef092be0e4af903a9e6629cdaac33832367b
switer <guankaishe@gmail.com>
2017-07-24 16:45:31 +0800

    annotation

--- comps/header.js
+++ comps/header.js
@@ -1,6 +1,9 @@
 var C = require('../lib/wxpage').C
 module.exports = C('header', function (vm) {
 	return {
+		/**
+		 * 子组件独立的数据
+		 */
 		data: {
 			title: 'header~'
 		},
@@ -9,17 +12,26 @@ module.exports = C('header', function (vm) {
 				title: 'Launch...'
 			})
 		},
+		/**
+		 * 子组件独立的生命周期方法
+		 */
 		onLoad: function () {
 			console.log('On header load', this.data)
 			setTimeout(function () {
 				vm.$set({'title': '√ Done'})
 			}, 1000)
 
+			/**
+			 * 子组件监听父组件消息
+			 */
 			vm.$on('play', function (playload) {
 				console.log(playload) // {cid: 123}
 			})
 
 		},
+		/**
+		 * 子组件事件处理方法
+		 */
 		onTap: function (e) {
 			console.log(e)
 		}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ var P = require('../lib/wxpage')
 var Header = require('../comps/header')
 
 P('index', {
+	/**
+	 * 注册子组件控制器
+	 */
 	comps: [
 		Header,
 	],
@@ -11,6 +14,9 @@ P('index', {
 	onLaunch: function () {
 		console.log('## On index page launch')
 	},
+	/**
+	 * 父组件声明周期方法
+	 */
 	onAppLaunch: function (opts) {
 		console.log('## [Page] APP Launch', opts)
 	},
@@ -23,8 +29,13 @@ P('index', {
 	onAwake: function (t) {
 		console.log('## On index page awake', t)
 	},
+	/**
+	 * 父组件事件回调
+	 */
 	onPlay: function () {
+		// 跳转其它页面
 		this.$route('play?cid=123')
-		this.$emitter('play', {cid: 123})
+		// 向子组件广播消息
+		this.$emitter.emit('play', {cid: 123})
 	}
 })
